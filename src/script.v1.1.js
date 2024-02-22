f70672e6237b14658a1f080c022c568e85c24c73
vladyslav nykoliuk <vladyslavn@yahoo.com>
2024-02-22 11:22:27 -0500

    version script.v1.2.js

--- src/script.v1.1.js
+++ src/script.v1.1.js
@@ -1,8 +1,8 @@
 var styles = `
     .ukraine {
         position: fixed;
-        bottom: 12px;
-        right: -108px;
+        bottom: 25px;
+        right: -75px;
         z-index: 999;
     }
     .ukraine #uawidget {
@@ -17,5 +17,5 @@ document.head.appendChild(styleSheet)
 
 const ishere = document.getElementById('ukraine') || false
 if (ishere) {
-    document.getElementById("ukraine").innerHTML = `<div class="ukraine"><a href="https://www.standwithukraine.dev/" target="_blank" title="United for Ukraine"><img src="https://i.imgur.com/XAGrqkr.png" alt="" id="uawidget"></a></div>`;
+    document.getElementById("ukraine").innerHTML = `<div class="ukraine"><a href="https://supportukrainenow.org/" target="_blank" title="United for Ukraine"><img src="https://i.ibb.co/PGYm5Rf/we-stand-with-ukraine-5.png" alt="" id="uawidget"></a></div>`;
 }
